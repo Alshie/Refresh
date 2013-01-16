1c43a0f9dc00a66088678537ed11de928df55bd3
Alan Wallace <alan@ginsoaked.com>
2013-01-16 17:15:19 +0000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// @codekit-prepend "vendor/modernizr.custom.97632.js";
+
+// analytics
+// var _gaq=[['_setAccount','UA-3746173-1'],['_trackPageview']];
+// (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+// g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+// s.parentNode.insertBefore(g,s)}(document,'script'));
